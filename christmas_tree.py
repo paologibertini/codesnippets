88e6042692e940c853943a0efb3324353cfd879f
paologibertini <71469104+paologibertini@users.noreply.github.com>
2022-02-21 10:58:55 +0100

    Update christmas_tree.py

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,7 +1,7 @@
 
 def main(x):
     # x is the tree high (trunk excluded)
-    albero = [f"{('*'*n):>{x-1}}" + "*" + f"{('*'*n):<{x-1}}" for n in (*range(x), 0, 0)]
+    albero = [f"{'*'*(2* n + 1):^{2*x+1}}" for n in (*range(x), 0, 0)]
     print("\n".join(albero))
 
 if __name__ == "__main__":
